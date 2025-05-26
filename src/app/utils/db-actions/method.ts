7a05443da4207db06b75ca7b43dbe010a8717cab
jakubkawka2005 <jakubkawka2005@gmail.com>
2025-05-26 11:32:56 +0000

    zwalidowanie danych po stronie serwera

--- src/app/utils/db-actions/method.ts
+++ src/app/utils/db-actions/method.ts
@@ -1,6 +1,8 @@
-'use server'
+"use server";
 
 import { Pool, QueryResult } from "pg";
+
+import pool from "../db";
 
 export interface MethodT {
   id: number;
@@ -10,36 +12,32 @@ export interface MethodT {
 }
 
 export async function getMethods(): Promise<MethodT[]> {
-    const client: Pool = new Pool();
-    const res: QueryResult = await client.query(
-      "SELECT id, name, cash, bank FROM public.method;"
-    );
-    await client.end();
-  
-    return res.rows.map((method) => ({
-      id: parseInt(method.id),
-      name: method.name,
-      cash: Boolean(method.cash),
-      bank: Boolean(method.bank),
-    }));
+  const client: Pool = new Pool();
+  const res: QueryResult = await client.query(
+    "SELECT id, name, cash, bank FROM public.method;"
+  );
+  await client.end();
+
+  return res.rows.map((method) => ({
+    id: parseInt(method.id),
+    name: method.name,
+    cash: Boolean(method.cash),
+    bank: Boolean(method.bank),
+  }));
 }
 
 export async function isCashMethod(id: number): Promise<boolean> {
-  const client: Pool = new Pool();
-  const res: QueryResult = await client.query(
+  const res: QueryResult = await pool.query(
     "SELECT id FROM public.method WHERE id = $1 AND cash = TRUE;",
     [id]
   );
-  await client.end();
   return res.rows.length > 0;
 }
 
 export async function isBankMethod(id: number): Promise<boolean> {
-  const client: Pool = new Pool();
-  const res: QueryResult = await client.query(
+  const res: QueryResult = await pool.query(
     "SELECT id FROM public.method WHERE id = $1 AND bank = TRUE;",
     [id]
   );
-  await client.end();
   return res.rows.length > 0;
 }
