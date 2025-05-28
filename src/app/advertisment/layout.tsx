e53a0a904e408a3f26cd35146529f1cdd7b5293b
jakubkawka2005 <jakubkawka2005@gmail.com>
2025-05-28 23:13:21 +0000

    zmiana podgladu

--- src/app/advertisment/layout.tsx
+++ src/app/advertisment/layout.tsx
@@ -3,7 +3,7 @@ import "@app/utils/globals.css";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "gringott",
+  title: "portfel",
   icons: {
     icon: "/favicon.png",
     shortcut: "/favicon.png",
@@ -20,9 +20,9 @@ export default async function Layout({
   return (
     <html lang="pl" className="font-noto">
       <body>
-        <div className="flex flex-col justify-center items-center gap-[14px] w-screen h-screen bg-gray-100">
-            <div className="justify-self-center self-center flex justify-center items-center font-bold text-gray-900 text-[100px] tracking-tight">
-              proste.
+        <div className="flex flex-col justify-center items-center gap-[14px] w-screen h-screen bg-surface-container">
+            <div className="justify-self-center self-center flex justify-center items-center font-bold text-primary text-[80px] tracking-tight">
+              prosty portfel.
             </div>
             {children}
         </div>
--- src/app/advertisment/page.tsx
+++ src/app/advertisment/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <div className="justify-self-center self-center flex justify-center items-center font-normal italic text-outline text-[32px] tracking-tight">
-      
+    <div className="justify-self-center self-center flex justify-center items-center font-normal italic text-outline text-[34px] tracking-tight">
+      ponieważ pieniądze mają znaczenie.
     </div>
   );
 }
