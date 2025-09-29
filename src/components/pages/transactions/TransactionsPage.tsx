0b3d269deea3dcac393902ab79340fd5300858d9
jakubkawka2005 <jakubkawka2005@gmail.com>
2025-09-29 20:58:11 +0000

    ulepszenie strony historycznej i listy transakcji

--- src/components/pages/transactions/TransactionsPage.tsx
+++ src/components/pages/transactions/TransactionsPage.tsx
@@ -28,24 +28,24 @@ export default function TransactionsPage({
   const router = useRouter();
 
   return (
-    <div className="flex flex-col w-[calc(100%-50px)] h-[calc(100%-50px)] bg-surface rounded-2xl shadow-sm">
+    <div className="flex flex-col w-[calc(100%-50px)] h-full">
       <div className="flex justify-center items-end gap-[20px] font-bold text-primary text-md w-full h-[50px] pb-[10px]">
-        <div className="flex justify-center items-center w-[200px]">DATA</div>
-        <div className="flex justify-center items-center w-[160px]">KWOTA</div>
-        <div className="flex justify-center items-center w-[200px]">OPIS</div>
+        <div className="flex justify-center items-center w-[20px]"></div>
+        <div className="flex justify-center items-center w-[200px]">data</div>
+        <div className="flex justify-center items-center w-[160px]">kwota</div>
+        <div className="flex justify-center items-center w-[200px]">opis</div>
+        <div className="flex justify-center items-center w-[200px]">z..</div>
         <div className="flex justify-center items-center w-[200px]">
-          DRUGA STRONA
-        </div>
-        <div className="flex justify-center items-center w-[200px]">
-          KATEGORIA
+          kategoria
         </div>
         <div className="flex justify-center items-center w-[120px]">
-          PORTFEL
+          portfel
         </div>
-        <div className="flex justify-center items-center w-[150px]">METODA</div>
+        <div className="flex justify-center items-center w-[150px]">metoda</div>
+        <div className="flex justify-center items-center w-[100px]"></div>
       </div>
       <div
-        className={`flex w-full h-[calc(100vh-160px)] pb-[30px] overflow-y-auto scroll-none ${
+        className={`flex w-full h-[calc(100vh-106px)] px-[20px] pb-[106px] overflow-y-auto scroll-none ${
           walletsReady && transactionsReady
             ? "flex-col"
             : "justify-center items-center"
@@ -65,7 +65,12 @@ export default function TransactionsPage({
         </div>
       </div>
       <div className="absolute bottom-10 right-10">
-        <Fab lowered onClick={() => router.push(`/${RouteSegments.Transactions}/${RouteSegments.New}`)}>
+        <Fab
+          lowered
+          onClick={() =>
+            router.push(`/${RouteSegments.Transactions}/${RouteSegments.New}`)
+          }
+        >
           <Icon slot="icon">add</Icon>
         </Fab>
       </div>
@@ -86,20 +91,22 @@ export function Transaction({
 
   return (
     <div
-      className={`flex justify-center items-center gap-[20px] font-normal text-on-surface-variant text-base w-full h-[30px] ${!transaction.important && 'opacity-100'}`}
+      className={`flex justify-center items-center gap-[20px] font-normal text-on-surface-variant text-base w-full h-[30px] border-y-[1px] border-surface-container hover:border-primary  ${
+        !transaction.important && "opacity-100"
+      }`}
       key={transaction.id}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
-      <div className="w-[70px] h-full"></div>
+      <div className="w-[20px] h-full"></div>
       <div className="flex justify-center items-center gap-[6px] w-[200px]">
         <div>{parseDate(transaction.date)}</div>
         <div className="text-[15px]">{parseTime(transaction.date)}</div>
       </div>
       <div
-        className={`flex justify-center items-center font-semibold text-lg w-[160px] ${
+        className={`flex justify-center items-center font-semibold text-xl w-[160px] ${
           transaction.income ? "text-green-800" : "text-red-800"
-        } ${!transaction.important && 'opacity-50'}`}
+        } ${!transaction.important && "opacity-50"}`}
       >
         {parseMoney(transaction.amount)} zł
       </div>
@@ -121,19 +128,27 @@ export function Transaction({
         {transaction.method.name}
       </div>
       <div
-        className={`flex justify-between items-center w-[70px] h-full transition-opacity ${
+        className={`flex justify-center items-center w-[100px] h-full transition-opacity ${
           hover ? "opacity-100" : "opacity-0"
         }`}
       >
         <IconButton
           className="mini"
-          onClick={() => router.push(`/${RouteSegments.Transactions}/${RouteSegments.Edit}/${transaction.id}`)}
+          onClick={() =>
+            router.push(
+              `/${RouteSegments.Transactions}/${RouteSegments.Edit}/${transaction.id}`
+            )
+          }
         >
           <Icon>edit</Icon>
         </IconButton>
         <IconButton
           className="mini error"
-          onClick={() => router.push(`/${RouteSegments.Transactions}/${RouteSegments.Delete}/${transaction.id}`)}
+          onClick={() =>
+            router.push(
+              `/${RouteSegments.Transactions}/${RouteSegments.Delete}/${transaction.id}`
+            )
+          }
         >
           <Icon>delete</Icon>
         </IconButton>
