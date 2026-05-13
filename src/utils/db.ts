6f45f137628920ef7b618fb615b4c5cb6faa8834
Jakub Kawka <jakubkawka2005@gmail.com>
2026-05-13 21:32:10 +0200

    zmiana bazy danych

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,13 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 const globalForPg = global as unknown as { pool: Pool };
 
-export const pool = globalForPg.pool || new Pool();
+export const pool =
+  globalForPg.pool ||
+  new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPg.pool = pool;
+if (process.env.NODE_ENV !== "production") globalForPg.pool = pool;
 
 export default pool;
